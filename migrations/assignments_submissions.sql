d7e786221b009ae42c2a4e1ea739775c6cbcff82
Cameron Brown <cameron-brown@live.com>
2020-11-24 20:29:29 +0000

    Add all Seeds

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -4,4 +4,9 @@ CREATE TABLE assignment_submissions (
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
-);
+);SELECT students.name AS student, AVG(assignment_submissions.duration) AS average_assignment_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+WHERE end_date IS NULL
+GROUP BY student
+ORDER BY average_assignment_duration DESC;
